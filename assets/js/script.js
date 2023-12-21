f82c0936770e2e19a132b32b2d2fafdd4124d9e8
elamont174 <>
2023-12-21 19:50:56 +0000

    Add function to run game for addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,29 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
-        });
+        })
     }
-});
+
+    runGame("addition");
+})
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
-   let num1 = Math.floor(Math.randon()*25)+1;
-   let num2 = Math.floor(Math.randon() * 25) + 1;
+function runGame(gameType) {
+   let num1 = Math.floor(Math.random()*25)+1;
+   let num2 = Math.floor(Math.random()*25) + 1;
+
+   if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+   } else {
+       alert(`Unknown game type: ${gameType}`);
+       throw `Unknown game type: ${gameType}. Aborting!`;
+   }
+
 }
 
 function checkAnswer() {
@@ -34,7 +44,10 @@ function incrementScore() {
 
 }
 
-function incrementWrongAnswer() {
+function incrementWrongAnswer(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
